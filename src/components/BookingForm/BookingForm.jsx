fbc87f4bdfd27243e42bb49e7a8c5e47d9c7d51d
artem-yaremchuk <artem.yaremchuk@yahoo.com>
2024-12-02 21:38:15 +0200

    Feat: Add custom DatePicker

--- src/components/BookingForm/BookingForm.jsx
+++ src/components/BookingForm/BookingForm.jsx
@@ -1,8 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
 import toast from "react-hot-toast";
 import css from "./BookingForm.module.css";
 
 const BookingForm = () => {
+  const [startDate, setStartDate] = useState(null);
+  const [placeholderText, setPlaceholderText] = useState("Booking date*");
+
+  const handleFocus = () => {
+    setPlaceholderText("Select a date between today");
+  };
+
+  const handleBlur = () => {
+    if (!startDate) {
+      setPlaceholderText("Booking date*");
+    }
+  };
+
   const handleSubmit = (e) => {
     e.preventDefault();
     toast.success(
@@ -36,13 +51,17 @@ const BookingForm = () => {
             placeholder="Email*"
             required
           ></input>
-          <input
-            className={css.inputDate}
-            type="date"
-            name="booking-date"
-            placeholder="Booking date*"
-            required
-          ></input>
+          <div className={css.inputDateWrapper}>
+            <DatePicker
+              className={css.inputDate}
+              selected={startDate}
+              onChange={(date) => setStartDate(date)}
+              placeholderText={placeholderText}
+              dateFormat="MMMM d, yyyy"
+              onFocus={handleFocus}
+              onBlur={handleBlur}
+            />
+          </div>
           <textarea
             className={css.commentBox}
             name="comment"
